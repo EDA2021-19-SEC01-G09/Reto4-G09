70a46643f7144ad8e3f352981a607a714934883b
Nathaliaq16 <85660281+Nathaliaq16@users.noreply.github.com>
2021-08-01 19:17:47 -0500

    Lab 10

--- App/model.py
+++ App/model.py
@@ -18,55 +18,159 @@
  *
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contribuciones:
+ *
+ * Dario Correal - Version inicial
  """
 
+
 import config as cf
-import model
-import csv
-
+from DISClib.ADT import list as lt
+from DISClib.ADT.graph import gr
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
+from DISClib.Algorithms.Graphs import scc
+from DISClib.Algorithms.Graphs import dijsktra as djk
+from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Utils import error as error
+assert cf
 
 """
-El controlador se encarga de mediar entre la vista y el modelo.
+Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
+los mismos.
 """
 
-# Inicialización 
+# Construccion de modelos
+def newcatalog():
+    catalog = { 
+                'countries': None,
+                'connections': None,
+                'points': None
+                }
+    catalog['points'] = mp.newMap(numelements=2000,
+                                     maptype='PROBING',
+                                     loadfactor= 0.5,
+                                     comparefunction=compareVerIds)
 
-def iniciar():
-    catalog = model.newcatalog()
+    catalog['connections'] = gr.newGraph(datastructure='ADJ_LIST',
+                                            directed=True,
+                                            size=5000,
+                                            comparefunction=compareVerIds)   
+
+    catalog['countries'] = lt.newList(datastructure='SINGLE_LINKED', cmpfunction = comparePais )    
+
     return catalog
 
-# Funciones para la carga de datos
+# Funciones para agregar informacion al catalogo
+def addPoint(catalog, point):
+    point['cables'] = None
+    mp.put(catalog['points'], point['landing_point_id'], point)
 
-def loadArchivos(catalog):
-    loadPoints(catalog)
-    loadConnec(catalog)
-    loadCountries(catalog)
 
+def addPointConne(catalog, coneccion):
+    try:
+        idorigen = coneccion['origin']
+        idedestino = coneccion['destination']
+        cable = coneccion['cable_id']
+        origen = formatVertex(idorigen, cable)
+        destino = formatVertex(idedestino, cable)
+        if 'n.a.' in coneccion['cable_length']:
+            distancia = 0.1
+        else:
+            distancia = float(coneccion['cable_length'].replace(',','').strip(' km'))
+        addVer(catalog, origen)
+        addVer(catalog, destino)
+        addConne(catalog, origen, destino, distancia)
+        addPointcable(catalog, idorigen,cable)
+        addPointcable(catalog, idedestino,cable)
+        return catalog
+    except Exception as exp:
+        error.reraise(exp, 'model:addPointConne')
+
+
+# Funciones para creacion de datos
+def addVer(catalog, pointid):
+    try:
+        if not gr.containsVertex(catalog['connections'], pointid):
+            gr.insertVertex(catalog['connections'], pointid)
+        return catalog
+    except Exception as exp:
+        error.reraise(exp, 'model:addVer')
+
+
+def addConne(catalog, origen, destino, distancia):
+    edge = gr.getEdge(catalog['connections'], origen, destino)
+    if edge is None:
+        gr.addEdge(catalog['connections'], origen, destino, distancia)
     return catalog
 
-def loadPoints(catalog):
-    archivo = cf.data_dir + 'landing_points.csv'
-    input_file = csv.DictReader(open(archivo, encoding='utf-8'), delimiter = ",")
-    
-    for file in input_file:
-        model.addPoint(catalog, file)
 
+def addPointcable(catalog, pointid, cable):
+    dato = mp.get(catalog['points'], pointid)
+    if dato['value']['cables'] is None:
+        ltcables = lt.newList(cmpfunction=comparecables)
+        lt.addLast(ltcables, cable)
+        dato['value']['cables'] = ltcables
+    else:
+        if not lt.isPresent(dato['value']['cables'], cable):
+            lt.addLast(dato['value']['cables'], cable)
+    return catalog
 
-def loadConnec(catalog):
-    archivo = cf.data_dir + 'connections.csv'
-    input_file = csv.DictReader(open(archivo, encoding='utf-8-sig'), delimiter = ",")
-    
-    for file in input_file:
-        model.addPointConne(catalog, file)
+def addCountry(catalog, country):
+    lt.addLast(catalog['countries'], country)
 
-def loadCountries(catalog):
-    archivo = cf.data_dir + 'countries.csv'
-    input_file = csv.DictReader(open(archivo, encoding='utf-8'), delimiter = ",")
-    
-    for file in input_file:
-        model.addCountry(catalog, file)
+# Funciones de consulta
 
-        
+# Funciones utilizadas para comparar elementos dentro de una lista
+
 # Funciones de ordenamiento
+def compareIds(id1, id2):
+    if (id1 == id2):
+        return 0
+    elif id1 > id2:
+        return 1
+    else:
+        return -1
 
-# Funciones de consulta sobre el catálogo
+def compareVerIds(ver, keyvaluever):
+    """
+    Compara dos estaciones
+    """
+    code = keyvaluever['key']
+    if (ver == code):
+        return 0
+    elif (ver > code):
+        return 1
+    else:
+        return -1
+
+def comparecables(cable1, cable2):
+
+    if (cable1 == cable2):
+        return 0
+    elif (cable1 > cable2):
+        return 1
+    else:
+        return -1
+
+def comparePais(pais1, pais2):
+    if (pais1['CountryName'] == pais2['CountryName']):
+        return 0
+    elif (pais1['CountryName'] > pais2['CountryName']):
+        return 1
+    else:
+        return -1
+def formatVertex(point, cable):
+
+    name = point + '-' + cable
+    return name
+
+def comparepoints(id, entry):
+    identry = me.getKey(entry)
+    if (id == identry):
+        return 0
+    elif (id > identry):
+        return 1
+    else:
+        return -1
